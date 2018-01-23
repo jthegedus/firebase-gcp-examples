f9f1ff6994afe365d846a797c1bf132fa0ad3526
jthegedus <jthegedus@jttech.io>
2018-01-23 23:27:48 +1100

    update deps

--- firebase-namespaced-packages/src/pages/index.js
+++ firebase-namespaced-packages/src/pages/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Link from "next/link";
-// everything
+// everything - except Firestore
 // import firebase from "firebase";
 
 // Individual exports & core lib
+// import messaging from "firebase/auth";
 // import database from "firebase/database";
 // import messaging from "firebase/messaging";
+// import messaging from "firebase/storage";
 
 // Scoped packages where core lib is explicit
-// import { firebase } from "@firebase/app";
-// import "@firebase/database";
-// import "@firebase/messaging";
+// @firebase                                  669.3 180.53
+// import { firebase } from "@firebase/app"; // 19.1   6.27
+// import "@firebase/auth"; // 134.81 43.42
+// import "@firebase/database"; // 193.34 47.15
+// import "@firebase/firestore";          // 212.9  50.64
+// import "@firebase/messaging"; // 19.66  5.63
+// import "@firebase/storage"; // 35.89  10.5
 
 export default class Index extends React.Component {
   static getInitialProps() {
