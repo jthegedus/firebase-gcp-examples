9b578ef4a447e77d28b4fdabd89ace95648f6d05
James Hegedus <james@jthegedus.com>
2019-05-12 19:02:25 +1000

    babel: update deps, scripts & use node 10

--- functions-w-babel/src/function.js
+++ functions-w-babel/src/function.js
@@ -3,12 +3,12 @@ import * as functions from 'firebase-functions';
 const message = () => {
   return new Promise(resolve => {
     setTimeout(() => {
-      resolve(`from compiled JavaScript via Babel on Cloud Functions!`);
+      resolve('from compiled JavaScript via Babel on Cloud Functions!');
     }, 3000);
   });
 };
 
-export let babel = functions.https.onRequest(async (req, res) => {
-  let world = await message();
+export const babel = functions.https.onRequest(async (req, res) => {
+  const world = await message();
   res.status(200).send(`Hello ${world}`);
 });
