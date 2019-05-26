6bf7057240bfe01c246635a35106780f1bc08689
James Hegedus <james@jthegedus.com>
2019-05-26 18:49:04 +1000

    nextjs: document server config

--- functions-nextjs/src/server/index.js
+++ functions-nextjs/src/server/index.js
@@ -2,7 +2,12 @@ import * as functions from "firebase-functions";
 import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev, conf: { distDir: "dist/client" } });
+const app = next({
+  dev,
+  // the absolute directory from the package.json file that initialises this module
+  // IE: the absolute path from the root of the Cloud Function
+  conf: { distDir: "dist/client" }
+});
 const handle = app.getRequestHandler();
 
 const server = functions.https.onRequest((request, response) => {
