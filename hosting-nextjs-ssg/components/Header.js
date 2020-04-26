d6bce89244407baefdc015ad5c1fbbe1e019a20e
jthegedus <james@jthegedus.com>
2020-04-26 14:08:05 +1000

    feat: nextjs ssg on firebase hosting

---
+++ hosting-nextjs-ssg/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+export default ({ pathname }) => (
+  <header>
+    <Link href="/">
+      <a className={pathname === "/" ? "is-active" : ""}>Home</a>
+    </Link>
+    <p/>
+    <Link href="/about">
+      <a className={pathname === "/about" ? "is-active" : ""}>About</a>
+    </Link>
+  </header>
+);
